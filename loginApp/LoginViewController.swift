a6995f5f5912030d38fd63513b770c6592eb296e
Stogov Anton <sloppybait@yandex.ru>
2023-03-17 01:06:19 +0300

    Removing redundant code

--- loginApp/LoginViewController.swift
+++ loginApp/LoginViewController.swift
@@ -7,17 +7,74 @@
 
 import UIKit
 
-class LoginViewController: UIViewController, UITextFieldDelegate {
-    
+class LoginViewController: UIViewController {
+
     @IBOutlet var passwordTextField: UITextField!
     @IBOutlet var loginTextField: UITextField!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        passwordTextField.delegate = self
         passwordTextField.isSecureTextEntry = true
     }
- 
+
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        guard let login = loginTextField.text, !login.isEmpty else {
+            showAlert(
+                title: "Ошибка",
+                message: "Пожалуйста, введите логин.")
+            return
+        }
+        guard let password = passwordTextField.text, !password.isEmpty else {
+            showAlert(
+                title: "Ошибка",
+                message: "Пожалуйста, введите пароль.")
+            return
+        }
+        if login == "Admin" && password == "Admin" {
+            performSegue(
+                withIdentifier: "openWelcomeVC",
+                sender: self)
+        } else {
+            showAlert(
+                title: "Ошибка",
+                message: "Неверный логин или пароль")
+        }
+    }
+
+    @IBAction func forgotLoginTapped(_ sender: Any) {
+        showInfoAlert(
+            title: "Ваш логин",
+            message: "Admin")
+    }
+
+    @IBAction func forgotPasswordTapped(_ sender: Any) {
+        showInfoAlert(
+            title: "Ваш пароль",
+            message: "Admin")
+    }
+
+    func showInfoAlert(title: String, message: String) {
+        let alertController = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(
+            title: "OK",
+            style: .default))
+        present(alertController, animated: true)
+    }
+
+    func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(
+            title: "ОК",
+            style: .default))
+        present(alertController, animated: true)
+    }
+
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "openWelcomeVC" {
             guard let secondVC = segue.destination as? WelcomeViewController else {
@@ -31,98 +88,12 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
         super.touchesBegan(touches, with: event)
         view.endEditing(true)
     }
-    
-    @IBAction func loginButtonTapped(_ sender: UIButton) {
-        guard let login = loginTextField.text, !login.isEmpty else {
-            let alertController = UIAlertController(
-                title: "Ошибка",
-                message: "Пожалуйста, введите логин.",
-                preferredStyle: .alert)
-            alertController.addAction(UIAlertAction(
-                title: "ОК",
-                style: .default,
-                handler: nil))
-            present(alertController,
-                    animated: true,
-                    completion: nil)
-            return
-        }
-        guard let password = passwordTextField.text, !password.isEmpty else {
-            let alertController = UIAlertController(
-                title: "Ошибка",
-                message: "Пожалуйста, введите пароль.",
-                preferredStyle: .alert)
-            alertController.addAction(UIAlertAction(
-                title: "ОК",
-                style: .default,
-                handler: nil))
-            present(alertController,
-                    animated: true,
-                    completion: nil)
-            return
-        }
-        
-        if login == "Admin" && password == "Admin" {
-        } else {
-            let alertController = UIAlertController(
-                title: "Ошибка",
-                message: "Неверный логин или пароль",
-                preferredStyle: .alert)
-            alertController.addAction(UIAlertAction(
-                title: "ОК",
-                style: .default,
-                handler: nil))
-            present(alertController,
-                    animated: true,
-                    completion: nil)
-        }
-    }
-    
-    @IBAction func forgotLoginTapped(_ sender: Any) {
-        showLoginAlert()
-    }
-    func getLogin() -> String? {
-        return "Admin"
-    }
-    func showLoginAlert() {
-        let login = getLogin()
-        let alertController = UIAlertController(
-            title: "Ваш логин",
-            message: login,
-            preferredStyle: .alert)
-        alertController.addAction(UIAlertAction(
-            title: "OK",
-            style: .default))
-        present(alertController,
-                animated: true,
-                completion: nil)
-    }
-    
+
     @IBAction func unwindToFirstViewController(_ unwindSegue: UIStoryboardSegue) {
         passwordTextField.text = ""
         loginTextField.text = ""
-        
-    }
-    
-    @IBAction func forgotPasswordTapped(_ sender: Any) {
-        showPasswordAlert()
-    }
-    func getPaswword() -> String? {
-        return "Admin"
-    }
-    func showPasswordAlert() {
-        let password = getPaswword()
-        let alertController = UIAlertController(
-            title: "Ваш пароль",
-            message: password,
-            preferredStyle: .alert)
-        alertController.addAction(UIAlertAction(
-            title: "OK",
-            style: .default))
-        present(alertController,
-                animated: true,
-                completion: nil)
     }
 
 }
+
 
