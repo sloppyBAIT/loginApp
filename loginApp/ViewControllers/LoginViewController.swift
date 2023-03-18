5dd9aafb32ca2118e5db09e244a86a928a47c069
Stogov Anton <sloppybait@yandex.ru>
2023-03-18 15:17:57 +0300

    Add tab bar and optimization code

--- loginApp/ViewControllers/LoginViewController.swift
+++ loginApp/ViewControllers/LoginViewController.swift
@@ -8,51 +8,54 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet var passwordTextField: UITextField!
     @IBOutlet var loginTextField: UITextField!
-
+    
+    let login = "Admin"
+    let password = "12345"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         passwordTextField.isSecureTextEntry = true
     }
-
+    
     @IBAction func loginButtonTapped(_ sender: UIButton) {
         guard let login = loginTextField.text, !login.isEmpty else {
-            showAlert(
+            showInfoAlert(
                 title: "Ошибка",
                 message: "Пожалуйста, введите логин.")
             return
         }
         guard let password = passwordTextField.text, !password.isEmpty else {
-            showAlert(
+            showInfoAlert(
                 title: "Ошибка",
                 message: "Пожалуйста, введите пароль.")
             return
         }
-        if login == "Admin" && password == "Admin" {
+        if login == self.login && password == self.password {
             performSegue(
                 withIdentifier: "openWelcomeVC",
                 sender: self)
         } else {
-            showAlert(
+            showInfoAlert(
                 title: "Ошибка",
                 message: "Неверный логин или пароль")
         }
     }
-
+    
     @IBAction func forgotLoginTapped(_ sender: Any) {
         showInfoAlert(
             title: "Ваш логин",
             message: "Admin")
     }
-
+    
     @IBAction func forgotPasswordTapped(_ sender: Any) {
         showInfoAlert(
             title: "Ваш пароль",
-            message: "Admin")
+            message: "12345")
     }
-
+    
     func showInfoAlert(title: String, message: String) {
         let alertController = UIAlertController(
             title: title,
@@ -63,18 +66,7 @@ class LoginViewController: UIViewController {
             style: .default))
         present(alertController, animated: true)
     }
-
-    func showAlert(title: String, message: String) {
-        let alertController = UIAlertController(
-            title: title,
-            message: message,
-            preferredStyle: .alert)
-        alertController.addAction(UIAlertAction(
-            title: "ОК",
-            style: .default))
-        present(alertController, animated: true)
-    }
-
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "openWelcomeVC" {
             guard let welcomeVC = segue.destination as? WelcomeViewController else {
@@ -83,17 +75,17 @@ class LoginViewController: UIViewController {
             welcomeVC.username = loginTextField.text
         }
     }
-
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
         view.endEditing(true)
     }
-
+    
     @IBAction func unwindToFirstViewController(_ unwindSegue: UIStoryboardSegue) {
         passwordTextField.text = ""
         loginTextField.text = ""
     }
-
+    
 }
 
 
